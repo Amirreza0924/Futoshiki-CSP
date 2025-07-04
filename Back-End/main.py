5f44fd92191db3d076164b240265d4236b07cfe8
Arman Akhoundy <iarmanprog@gmail.com>
2025-07-04 17:10:51 +0330

    Add HTTP exception handling for successful problem solving in solve_problem endpoint

--- Back-End/main.py
+++ Back-End/main.py
@@ -86,6 +86,10 @@ async def solve_problem(problem_data: problem):
             "message": "Solotion Khedmat Shoma!",
             "solution": solution
         }
+    
+    except HTTPException as http_exc:
+        raise http_exc
+    
     except Exception as e:
         raise HTTPException(
             status_code=500,
